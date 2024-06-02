6be95a5be27fafbe7e6f6ac4a2b3e5766d0908de
Carlobellido <carlo_del_piero@hotmail.com>
2024-06-02 21:09:29 +0200

    modifiche e fix bugs

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 let secretNumber = Math.trunc((Math.random() * 20) + 1);
 let score = 20;
 const guessRestore = document.querySelector('.message').textContent
+let highscore=0;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -19,6 +20,11 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.number').textContent = secretNumber ;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
+
+        if(score> highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent= highscore;
+        } 
 
         //Quando il l'input è più grande del Numero da indovinare
     } else if (guess > secretNumber) {
@@ -47,13 +53,13 @@ document.querySelector('.check').addEventListener('click', function () {
 })
 
 document.querySelector('.again').addEventListener('click', function(){
-document.querySelector('.score').textContent = '20'
+    score = 20
+document.querySelector('.score').textContent = score
 document.querySelector('.number').textContent = '?'
 document.querySelector('body').style.backgroundColor = '#222';
-document.querySelector('.guess').value = null
+document.querySelector('.guess').value = ""
 document.querySelector('.message').textContent = guessRestore
 document.querySelector('.number').style.width = '15rem'
 secretNumber = Math.trunc((Math.random() * 20) + 1);
 
 })
-
